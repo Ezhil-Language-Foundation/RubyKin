ca99080408b38b543eee65c16910ebb1397a6ab9
Muthiah Annamalai <ezhillang@gmail.com>
2019-05-11 19:25:20 -0700

    Revert "/Users/muthu/devel/RubyKin$python stats.py"
    This reverts commit 42e59d111750d261291d16fbf7bac2fd131cd53f.
    Wrong wrong commit. F-ed up the git commit.

---
+++ SUMMARY.md
@@ -0,0 +1,36 @@
+# Summary
+
+1. [நிரலாக்கம் என்றல் என்ன ?](what_is_programming/intro.md)
+   1. [கட்டளை கணம்](what_is_programming/instruction_set.md)
+2. [எண்கள்](numbers/number.md)
+   1. [பயிற்சி](numbers/practice.md)
+3. [சரம்](strings/string.md)
+   1. [பயிற்சி](strings/practice.md)
+4. [மாறிலி](variables/variable.md)
+   1. [பயிற்சி](variables/practice.md)
+5. [ஆனால் நிபந்தனை கட்டளை](if_and_else/conditionals.md)
+   1. [பயிற்சி](if_and_else/practice.md)
+6. [மடக்கு கட்டளை](loops/loop.md)
+   1. [பயிற்சி](loops/practice.md)
+7. [தரவமைப்பு வகைகள்](collections/storage.md)
+   1. [அணிகள்](collections/array.md)
+   2. [தோடர்புரு அணி/எண்அடைவு](collections/hash.md)
+   3. [பயிற்சி](collections/practice.md)
+
+8. [சார்பு நிரல்பாகம்](methods/functions.md)
+   1. [பயிற்சி](methods/practice.md)
+
+9. [எண்வகை தரவமைப்புகள்](enumerables/enumerable.md)
+   1. [ஒவ்வொன்றாக](enumerables/each.md)
+   2. [ஒவ்வொன்றாக இடம்காட்டியுடன்](enumerables/each_with_index.md)
+   3. [தேர்ந்தெடு](enumerables/select.md)
+   4. [தொடர்புறு அணி](enumerables/map.md) 
+   5. [பயிற்சி](enumerables/practice.md)
+
+10. [கோப்புகள்](files/file.md)
+   1. [முடிவுகள்](files/the_end.md)
+
+# பின் இணப்புகள் (Appendix)
+அ. [மாதிரி பயிற்சி விடைகள்](answers_to_practice_problems/answers.md)
+
+ஆ. [இணைப்பு](appendix/contact.md)
--- stats.py
+++ stats.py
@@ -24,24 +24,19 @@ def get_words(filename):
                 njunk+=1
         letters = tamil.utf8.get_letters(data)
         ntamil = len( tamil.utf8.get_tamil_words( letters ) )
-        return (max(ntamil,nwords-njunk),ntamil)
+        return (nwords-njunk,ntamil)
     return (0,0)
 
+fd_perc = {}
 def process(fd):
     if os.path.isdir(fd):
         for f_or_d in glob.glob(os.path.join(fd,'*')):
             if os.path.isdir( f_or_d): 
-                for x in process( f_or_d ):
-                    yield x
+                process( f_or_d )
             else:
-                yield f_or_d
+                fd_perc[f_or_d] = handlefile(f_or_d)
     else:
-        yield fd
-
-def start(s_dir):
-    md_files = filter(lambda x: x.endswith('.md'),list(process(s_dir)))
-    fd_perc = { fd: handlefile(fd) for fd in md_files }
-    return fd_perc
+        fd_perc[fd] = handlefile(fd)
 
 def handlefile(fd):
     if fd.endswith(u".md"):
@@ -51,12 +46,13 @@ def handlefile(fd):
         FLIST.append( [fd, {'lang_eng':a,'lang_tam':b,'perc':b/float(a+b)}] )
         #print(u"%s => %f %%"%(fd.replace(basename,''),100.0*float(b)/(a+b)))
         return 100.0*float(b)/float(a+b)
-    return 0.0
+    return -1.0
 
 basename = ''
 if __name__ == u"__main__":
+    fd_perc.clear()
     basename = os.getcwd()
-    fd_perc  = start(os.getcwd())
+    process(os.getcwd())
 
     fd_perc = [(k,v) for k,v in fd_perc.items()] 
     fd_perc2 = sorted(fd_perc,key=itemgetter(1))
